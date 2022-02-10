42cac36d69628a4aa1d47e86e28d5d090fd0ade2
Derick Vigne <derickvigne@me.com>
2022-02-09 19:33:18 -0500

    Updated to Python3

--- discover_files.py
+++ discover_files.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os,json,sys,re
 from base64 import b64decode
@@ -6,11 +6,11 @@ from base64 import b64decode
 data = []
 
 if len(sys.argv)<3:
-  print json.dumps({'data':data,'error':'Usage: discover_files.py path b64_fname_regex'})
+  print(json.dumps({'data':data,'error':'Usage: discover_files.py path b64_fname_regex'}))
   sys.exit(1)
 
 if not os.path.isdir(sys.argv[1]):
-  print json.dumps({'data':data,'error': sys.argv[1]+' is not a directory'})
+  print(json.dumps({'data':data,'error': sys.argv[1]+' is not a directory'}))
   sys.exit(1)
 
 def find_all(name, path):
@@ -19,13 +19,13 @@ def find_all(name, path):
   try:
     name=b64decode(name)
   except Exception as e:
-    print json.dumps({'data':data,'error': 'Can\'t decode '+name+' as base64. '+str(e)})
+    print(json.dumps({'data':data,'error': 'Can\'t decode '+name+' as base64. '+str(e)}))
     sys.exit(1)
   # try regex
   try:
     reobj = re.compile(name)
   except Exception as e:
-    print json.dumps({'data':data,'error': 'Can\'t compile '+name+' as regex. '+str(e)})
+    print(json.dumps({'data':data,'error': 'Can\'t compile '+name+' as regex. '+str(e)}))
     sys.exit(1)
   for root, dirs, files in os.walk(path):
     for fname in files:
@@ -36,4 +36,4 @@ def find_all(name, path):
 for fpath in find_all(sys.argv[2],sys.argv[1]):
   data.append({'{#FDIR}':os.path.dirname(fpath),'{#FDIRNAME}':os.path.basename(os.path.dirname(fpath)),'{#FPATH}':fpath,'{#FNAME}':os.path.basename(fpath)})
 
-print json.dumps({'data':data})
+print(json.dumps({'data':data}))
